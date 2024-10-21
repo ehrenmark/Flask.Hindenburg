41bd31b782fbde89de99569b3080376275d71ba3
Mark Kagermeier <95859677+ehrenmark@users.noreply.github.com>
2024-10-21 15:50:37 +0200

    asdf

--- flaskProject/map.py
+++ flaskProject/map.py
@@ -1,8 +1,10 @@
 from datetime import datetime, timedelta
-
+import sqlite3
+import requests
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
 from models import db
+from models import Airports
 from sqlalchemy import or_, and_
 import json
 
@@ -14,8 +16,12 @@ map = Blueprint('map', __name__)
 def map_function():
 
 
+    airports = db.session.query(Airports).all()
+    print(airports)
+
+
+
     return render_template("map2.html",
                            user=current_user,
+                           airports=airports,
                            )
-
-
--- flaskProject/models.py
+++ flaskProject/models.py
@@ -15,3 +15,18 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
+
+
+class Airports(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    Name = db.Column(db.String(150))
+    IATA = db.Column(db.String(150))
+    ICAO = db.Column(db.String(150))
+    Country = db.Column(db.String(150))
+    Continent = db.Column(db.String(150))
+    Passengers = db.Column(db.Integer)
+    Cargo = db.Column(db.Integer)
+    altitude = db.Column(db.Integer)
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
+
